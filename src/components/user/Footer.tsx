cfbea6c3a1905c6d11772f1108d885924b787f44
Daniel Akinbo <olasocials@gmail.com>
2024-04-11 20:49:27 +0100

    footer changes

--- src/components/user/Footer.tsx
+++ src/components/user/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <section className="bg-white px-4 pt-24 relative">
+    <section className="bg-white px-4 pt-24 relative pverflow-hidden">
       <div className="flex flex-col lg:flex-row-reverse justify-around">
         <div className="gap-6 flex flex-col mb-12">
           <div className="flex gap-2 lg:gap-4 items-center justify-center w-fit">
